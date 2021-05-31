7279cadaf8486f349fffb6a165f213aede2e6b9d
ykurochkin <zhenia.kurochkin@gmail.com>
2021-05-31 17:09:01 +0300

    upgrade version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-salesforce',
-      version='1.4.37',
+      version='1.4.38-airbyte',
       description='Singer.io tap for extracting data from the Salesforce API',
       author='Stitch',
       url='https://singer.io',
